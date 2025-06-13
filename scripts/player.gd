9adec8f0ec7e206d7d1b81320ac8010d638b4e64
Tomas Svojanovsky <tomas.svojanovsky33@gmail.com>
2025-06-13 10:43:35 +0200

    Add run

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,21 +1,43 @@
 extends CharacterBody2D
 
 const GRAVITY: float = 690.0
+const RUN_SPEED: float = 120.0
 
-enum State { IDLE }
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+enum State { IDLE, RUN }
 var current_state: State = State.IDLE
 var new_state: State
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+	pass
+	
+func _physics_process(delta: float) -> void:
 	velocity.y += GRAVITY * delta
 	
+	get_input()
 	update_animation()
+	move_and_slide()
+	
+func get_input() -> void:
+	var horizontal_input = Input.get_axis("move_left", "move_right")
+
+	velocity.x = RUN_SPEED * horizontal_input
+
+	if not is_equal_approx(velocity.x, 0.0):
+		animated_sprite_2d.flip_h = velocity.x < 0
 
 func update_animation() -> void:
 	if is_on_floor():
-		new_state = State.IDLE
+		if abs(velocity.x) > 10:
+			new_state = State.RUN
+		else:
+			new_state = State.IDLE
+			
+	if new_state != current_state:
+		current_state = new_state
+		match current_state:
+			State.IDLE:
+				animated_sprite_2d.play("idle")
+			State.RUN:
+				animated_sprite_2d.play("run")
